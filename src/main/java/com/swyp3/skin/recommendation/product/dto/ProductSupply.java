8e10df57733a7f04ddcd7545ea760a97cf84ea04
윤민수 <koelysa5696@naver.com>
2026-05-05 10:18:26 +0900

    feat: 상품 카테고리를 추가

--- src/main/java/com/swyp3/skin/recommendation/product/dto/ProductSupply.java
+++ src/main/java/com/swyp3/skin/recommendation/product/dto/ProductSupply.java
@@ -1,6 +1,7 @@
 package com.swyp3.skin.recommendation.product.dto;
 
 import com.swyp3.skin.domain.common.enums.IngredientGroup;
+import com.swyp3.skin.domain.product.domain.enums.ProductCategory;
 import lombok.Getter;
 
 import java.util.Map;
@@ -9,10 +10,12 @@ import java.util.Map;
 public class ProductSupply {
 
     private final Long productId;
+    private final ProductCategory productCategory;
     private final Map<IngredientGroup, Double> supply;
 
-    public ProductSupply(Long productId, Map<IngredientGroup, Double> supply) {
+    public ProductSupply(Long productId, ProductCategory productCategory, Map<IngredientGroup, Double> supply) {
         this.productId = productId;
+        this.productCategory = productCategory;
         this.supply = supply;
     }
 }
