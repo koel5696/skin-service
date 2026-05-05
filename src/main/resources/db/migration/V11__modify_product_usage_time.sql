cd94fc5fed43708cdc31a10f0662d50adb57d287
윤민수 <koelysa5696@naver.com>
2026-05-05 11:12:57 +0900

    feat: 선크림을 전부 am으로 바꾸고 am만 허용하도록 체크

--- src/main/resources/db/migration/V11__modify_product_usage_time.sql
+++ src/main/resources/db/migration/V11__modify_product_usage_time.sql
@@ -1,2 +1,10 @@
+UPDATE product
+SET product_usage_time = 'AM'
+WHERE category = 'SUN_CARE';
+
 ALTER TABLE product
     MODIFY COLUMN product_usage_time VARCHAR(10) NOT NULL;
+
+ALTER TABLE product
+    ADD CONSTRAINT chk_product_sun_care_usage_time
+        CHECK (category <> 'SUN_CARE' OR product_usage_time = 'AM');
